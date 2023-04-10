ed6b97b7ff4cb4181015ec5988e583d5eb83e053
Yuliy Kisilyova <kisilyovayulia@gmail.com>
2023-04-10 19:23:53 +0300

    testing committing

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yuliiakisilova
+ *
+ */
+module CarManager {
+}
---
+++ src/ua/com/foxminded/carmanager/CarManager.java
@@ -0,0 +1,9 @@
+package ua.com.foxminded.carmanager;
+
+public class CarManager {
+
+	public static void main(String[] args) {
+
+	}
+
+}
